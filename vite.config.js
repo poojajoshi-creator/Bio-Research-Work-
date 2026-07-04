e7870c570278804d81606d92916b6ddfac5fb682
poojajoshi-creator <poojajoshi12@gmail.com>
2026-07-04 14:51:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// If deploying to GitHub Pages at https://<user>.github.io/<repo>/,
-// set base to "/<repo>/". If deploying to a custom domain or Vercel/Netlify,
-// leave it as "/".
 export default defineConfig({
   plugins: [react()],
-  base: "/fsgs-research-portal/",
+  base: "/Bio-Research-Work-/",
 });
